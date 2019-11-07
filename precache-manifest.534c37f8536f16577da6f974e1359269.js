b1095e54e81584fddf911d876d1d3d2768381d51
AlexeyDi <dialex-u@mail.ru>
2019-11-07 10:36:25 +0400

    Updates

---
+++ precache-manifest.534c37f8536f16577da6f974e1359269.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4671554c329684f2e4e902a69d09cba8",
+    "url": "/repo-for-react/index.html"
+  },
+  {
+    "revision": "63fe6475f28e64577ea5",
+    "url": "/repo-for-react/static/css/main.9a01c8b0.chunk.css"
+  },
+  {
+    "revision": "8b0aa7f835768fdefd35",
+    "url": "/repo-for-react/static/js/2.ff4e7eea.chunk.js"
+  },
+  {
+    "revision": "63fe6475f28e64577ea5",
+    "url": "/repo-for-react/static/js/main.163440ba.chunk.js"
+  },
+  {
+    "revision": "3ce8d1c9a505b36b2a4f",
+    "url": "/repo-for-react/static/js/runtime-main.29edd94d.js"
+  }
+]);
